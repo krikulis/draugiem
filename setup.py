448b24615449d63d585f200d0220e54c2ded9723
Kristaps Kulis <Kristaps.Kulis@gmail.com>
2011-02-01 16:16:03 +0200

    adding setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup 
+setup(
+    name = "draugiem",
+    packages = ["draugiem"],
+    version = "0.1",
+    description = "draugiem.lv api wrapper",
+    author = "Kristaps Kulis",
+    author_email = "kristaps.kulis@gmail.com",
+    url = "http://www.github.com/krikulis/draugiem",
+    classifiers = [
+        'Development Status :: 5 - Alfa',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+    ],
+    )
